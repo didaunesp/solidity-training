247497ac440bb93443df4eda0301571add08aa09
Guilherme Lopes Ferreira <gferreira525@gmail.com>
2022-01-24 18:05:16 -0300

    constructor and setMessage tests

--- boilerplate-inbox/test/Inbox.test.js
+++ boilerplate-inbox/test/Inbox.test.js
@@ -5,25 +5,33 @@ const web3 = new Web3(ganache.provider())
 const { interface, bytecode } = require('../compile')
 
 let inbox
+let accounts
 
 beforeEach(async () => {
   // Get a list of all accounts
-  const accounts = await web3.eth.getAccounts()
+  accounts = await web3.eth.getAccounts()
 
   console.log(accounts[0])
 
   // Use one of those accounts to deploy the contract
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there"!'] })
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
     .send({ from: accounts[0], gas: '1000000' })
 })
 
-describe('First Test', async () => {
-  it('Fist assertion', async () => {
-    assert.equal(true, true)
+describe('Inbox testing', async () => {
+  it('Deploys a contract', () => {
+    assert.ok(inbox.options.address)
   })
 
-  it('Second assertion', async () => {
-    assert.equal(false, true)
+  it('Initial Message', async () => {
+    const message = await inbox.methods.message().call()
+    assert.equal(message, 'Hi there!')
+  })
+
+  it('Set Message', async () => {
+    await inbox.methods.setMessage('New Message').send({ from: accounts[0] })
+    const message = await inbox.methods.message().call()
+    assert.equal(message, 'New Message')
   })
 })
