234dff3bdf76e8f32ef81879d5d1aef39e1e4459
Guilherme Lopes Ferreira <gferreira525@gmail.com>
2022-01-24 15:44:40 -0300

    Add prettier and eslint

--- boilerplate-inbox/compile.js
+++ boilerplate-inbox/compile.js
@@ -1,1 +1,9 @@
-// compile code will go here
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, '..', 'contracts', '0.4', 'Inbox.sol');
+
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+module.exports = solc.compile(source, 1).contracts[':Inbox'];
--- boilerplate-inbox/test/Inbox.test.js
+++ boilerplate-inbox/test/Inbox.test.js
@@ -1,1 +1,29 @@
-// contract test code will go here
+const assert = require('assert')
+const ganache = require('ganache-cli')
+const Web3 = require('web3')
+const web3 = new Web3(ganache.provider())
+const { interface, bytecode } = require('../compile')
+
+let inbox
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  const accounts = await web3.eth.getAccounts()
+
+  console.log(accounts[0])
+
+  // Use one of those accounts to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there"!'] })
+    .send({ from: accounts[0], gas: '1000000' })
+})
+
+describe('First Test', async () => {
+  it('Fist assertion', async () => {
+    assert.equal(true, true)
+  })
+
+  it('Second assertion', async () => {
+    assert.equal(false, true)
+  })
+})
