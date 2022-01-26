2723be50b6386adfa5584bfb03c6dd67edb76fbd
Guilherme Lopes Ferreira <gferreira525@gmail.com>
2022-01-26 17:03:01 -0300

    Add get players function

--- solidity-0.8/contracts/Lottery.sol
+++ solidity-0.8/contracts/Lottery.sol
@@ -11,17 +11,23 @@ contract Lottery {
     }
 
     function enter() public payable {
-        require(msg.value == .01 ether);
+        require(msg.value == .01 ether, "Minimum value to enter is 10000000 gwei");
         players.push(msg.sender);
     }
     
     function pickWinner() public payable {
-        require(msg.sender == manager);
+        require(msg.sender == manager, "Only manager address can pick a winner");
 
         uint index = (random() % players.length);
         address payable self = payable(address(this));
         address payable winner = payable(players[index]);
         winner.transfer(self.balance);
+
+        players = new address[](0);
+    }
+
+    function getPlayers() public view returns(address[] memory){
+        return players;
     }
 
     function random() private view returns(uint){
